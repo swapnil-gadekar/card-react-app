9702bc6db45936fe9081b56277dee444a5b0e649
swapnil-gadekar <swapnil.gdkr96@gmail.com>
2020-05-12 11:28:46 +0530

    Updates

---
+++ precache-manifest.08b4aaa8729c8684dd2bca356fe00c37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eded64cd7d8b2ea0da719df9d39cd078",
+    "url": "/card-react-app/index.html"
+  },
+  {
+    "revision": "625278a72280ca6e4b7b",
+    "url": "/card-react-app/static/css/main.1832d120.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/card-react-app/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card-react-app/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "625278a72280ca6e4b7b",
+    "url": "/card-react-app/static/js/main.4f63e348.chunk.js"
+  },
+  {
+    "revision": "9e2130a02075a87e0120",
+    "url": "/card-react-app/static/js/runtime-main.7c300219.js"
+  }
+]);
